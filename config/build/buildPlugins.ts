efbd82b74e32c0aa45328f95fd299346c514a426
Николай Горичев <nikogorichev812@gmail.com>
2024-03-27 21:55:39 +0300

    add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,11 +1,12 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { BuildOptions } from "./types/config";
 import MiniCssExctractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({
   paths,
-  isDev
+  isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HtmlWebpackPlugin({
@@ -14,12 +15,14 @@ export function buildPlugins({
     new webpack.ProgressPlugin(),
     new MiniCssExctractPlugin({
       filename: "scc/[name].[contenthash:8].css",
-      chunkFilename: "scc/[name].[contenthash:8].css"
+      chunkFilename: "scc/[name].[contenthash:8].css",
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-     
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    }),
   ];
 }

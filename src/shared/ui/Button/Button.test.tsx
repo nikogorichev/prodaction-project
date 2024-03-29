77a4815e01e953a77a8198c76b9c788a9f33b74a
Николай Горичев <nikogorichev812@gmail.com>
2024-03-29 19:10:51 +0300

    add test for button.tsx clear theme

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import { Button } from "./Buttons";
+import { Button, ThemeButton } from "./Buttons";
 import React from "react";
 
 describe("Button", () => {
-  test("render", () => {
+  test("initial render", () => {
     render(<Button>TEST</Button>);
     expect(screen.getByText("TEST")).toBeInTheDocument();
+  });
+  
+  test("render button with clear theme", () => {
+    render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
+    expect(screen.getByText("TEST")).toHaveClass("clear");
+    screen.debug();
   });
 });

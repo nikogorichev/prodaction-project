1010c2680418ae63e555e8c5359ce4961279255a
Николай Горичев <nikogorichev812@gmail.com>
2024-04-22 18:22:45 +0300

    add json server

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, screen } from "@testing-library/react";
 import { Counter } from "./Counter";
 import { componentRender } from "shared/lib/tests/componentRender/componentRender";
-import { userEvent } from "@storybook/test";
+// import { userEvent } from '@storybook/testing-library';
 
 describe("Counter", () => {
   test("counter in document", () => {
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -11,60 +11,57 @@ interface ModalProps {
 
 const ANIMATION_DELAY = 300;
 
-export const Modal: React.FC<ModalProps> = memo(({
-  className,
-  children,
-  isOpen,
-  onClose,
-}) => {
-  const [isClosing, setIsClosing] = useState(false);
-  const timerRef = useRef<ReturnType<typeof setTimeout>>();
+export const Modal: React.FC<ModalProps> = memo(
+  ({ className, children, isOpen, onClose }) => {
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
 
-  const mods: Record<string, boolean> = {
-    [styles.opened]: isOpen,
-    [styles.isClosing]: isClosing,
-  };
+    const mods: Record<string, boolean> = {
+      [styles.opened]: isOpen,
+      [styles.isClosing]: isClosing,
+    };
 
-  const handleOnClose = () => {
-    if (onClose) {
-      setIsClosing(true);
-      timerRef.current = setTimeout(() => {
-        onClose();
-        setIsClosing(false);
-      }, ANIMATION_DELAY);
-    }
-  };
-
-  useEffect(() => {
-    const onKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        handleOnClose();
+    const handleOnClose = () => {
+      if (onClose) {
+        setIsClosing(true);
+        timerRef.current = setTimeout(() => {
+          onClose();
+          setIsClosing(false);
+        }, ANIMATION_DELAY);
       }
     };
-    if (isOpen) {
-      window.addEventListener("keydown", onKeyDown);
-    }
-    return () => {
-      clearTimeout(timerRef.current);
-      window.removeEventListener("keydown", onKeyDown);
+
+    useEffect(() => {
+      const onKeyDown = (event: KeyboardEvent) => {
+        if (event.key === "Escape") {
+          handleOnClose();
+        }
+      };
+      if (isOpen) {
+        window.addEventListener("keydown", onKeyDown);
+      }
+      return () => {
+        clearTimeout(timerRef.current);
+        window.removeEventListener("keydown", onKeyDown);
+      };
+    }, [isOpen]);
+
+    const onContentClick = (event: React.MouseEvent) => {
+      event.stopPropagation();
     };
-  }, [isOpen]);
 
-  const onContentClick = (event: React.MouseEvent) => {
-    event.stopPropagation();
-  };
-
-  return (
-    <Portal>
-      <div className={classNames(styles.modal, mods, [className])}>
-        <div className={styles.overlay} onClick={handleOnClose}>
-          <div className={styles.content} onClick={onContentClick}>
-            {children}
+    return (
+      <Portal>
+        <div className={classNames(styles.modal, mods, [className])}>
+          <div className={styles.overlay} onClick={handleOnClose}>
+            <div className={styles.content} onClick={onContentClick}>
+              {children}
+            </div>
           </div>
         </div>
-      </div>
-    </Portal>
-  );
-});
+      </Portal>
+    );
+  }
+);
 
-Modal.displayName = "Modal"
+Modal.displayName = "Modal";

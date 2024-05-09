2a2daab1a3c7220b49c1923784df9e8e6ba9a245
Николай Горичев <nikogorichev812@gmail.com>
2024-05-09 14:11:30 +0300

    add test for login slice

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,30 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { LoginSchema } from "../types/loginSchema";
+import { loginActions, loginReducer } from "./loginSlice";
+import { loginByUsername } from "../services/loginByUsername/loginByUsername";
+
+describe("loginSlice", () => {
+  test("should record username", () => {
+    const state: DeepPartial<LoginSchema> = {
+      username: "",
+    };
+
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setUserName("admin"))
+    ).toEqual({
+      username: "admin",
+    });
+  });
+
+  test("should record password", () => {
+    const state: DeepPartial<LoginSchema> = {
+      password: "",
+    };
+
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setPassword("123"))
+    ).toEqual({
+      password: "123",
+    });
+  });
+});

62bab0153f92212b411377a15b57c611f69f86e9
Николай Горичев <nikogorichev812@gmail.com>
2024-05-09 11:10:32 +0300

    add test for selectors auth

---
+++ src/features/AuthByUsername/model/selectors/getLoginError/getLoginError.test.ts
@@ -0,0 +1,16 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { StateSchema } from "app/providers/StoreProvider";
+import { getLoginError } from "./getLoginError";
+
+describe("getLoginError", () => {
+  test("should return error", () => {
+    const state: DeepPartial<StateSchema> = {
+      loginForm: { error: "test error" },
+    };
+    expect(getLoginError(state as StateSchema)).toEqual("test error");
+  });
+  test("should work with empty state", () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginError(state as StateSchema)).toEqual("");
+  });
+});
---
+++ src/features/AuthByUsername/model/selectors/getLoginLoading/getLoginLoading.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { StateSchema } from "app/providers/StoreProvider";
+import { getLoginLoading } from "./getLoginLoading";
+
+describe("getLoginLoading", () => {
+  test("should return true while loading", () => {
+    const state: DeepPartial<StateSchema> = { loginForm: { isLoading: true } };
+    expect(getLoginLoading(state as StateSchema)).toEqual(true);
+  });
+  test("should return false", () => {
+    const state: DeepPartial<StateSchema> = { loginForm: { isLoading: false } };
+    expect(getLoginLoading(state as StateSchema)).toEqual(false);
+  });
+  test("should return false with empty state", () => {
+    const state: DeepPartial<StateSchema> = {};
+    expect(getLoginLoading(state as StateSchema)).toEqual(false);
+  });
+});

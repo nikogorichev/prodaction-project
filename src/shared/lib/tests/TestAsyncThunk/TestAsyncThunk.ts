c000b8abda55ea8ba070a98ce9ad3b0fc3fca428
Николай Горичев <nikogorichev812@gmail.com>
2024-06-01 16:30:57 +0300

    fix ts error

--- src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
+++ src/shared/lib/tests/TestAsyncThunk/TestAsyncThunk.ts
@@ -1,5 +1,6 @@
 import { AsyncThunkAction } from "@reduxjs/toolkit";
 import { StateSchema } from "app/providers/StoreProvider";
+import axios, { AxiosStatic } from "axios";
 
 type ActionCreatorType<Return, Arg, RejectedValue> = (
   arg: Arg
@@ -10,20 +11,33 @@ type ActionCreatorType<Return, Arg, RejectedValue> = (
     rejectValue: RejectedValue;
   }
 >;
+
+jest.mock("axios");
+
+const mockedAxios = jest.mocked(axios, true);
 export class TestAsyncThunk<Return, Arg, RejectedValue> {
   dispatch: jest.MockedFn<any>;
   getState: () => StateSchema;
   actionCreator: ActionCreatorType<Return, Arg, RejectedValue>;
 
+  api: jest.MockedFunctionDeep<AxiosStatic>;
+  navigate: jest.MockedFn<any>;
+
   constructor(actionCreator: ActionCreatorType<Return, Arg, RejectedValue>) {
     this.actionCreator = actionCreator;
     this.dispatch = jest.fn();
     this.getState = jest.fn();
+
+    this.api = mockedAxios;
+    this.navigate = jest.fn();
   }
 
   async callThunk(arg: Arg) {
     const action = this.actionCreator(arg);
-    const result = await action(this.dispatch, this.getState, undefined);
+    const result = await action(this.dispatch, this.getState, {
+      api: this.api,
+      navigate: this.navigate,
+    });
 
     return result;
   }

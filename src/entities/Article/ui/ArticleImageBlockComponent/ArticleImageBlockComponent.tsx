ec160734820b0e6b137bf8bef0e404553d4d2f7b
Николай Горичев <nikogorichev812@gmail.com>
2024-10-05 16:21:24 +0300

    add image block component

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -1,10 +1,21 @@
+import { Text, TextAlign } from "shared/ui/Text/Text";
 import { ArticleImageBlock } from "../../model/types/article";
+import styles from "./ArticleImageBlockComponent.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
 
 type Props = {
-  block: ArticleImageBlock
+  block: ArticleImageBlock;
   className?: string;
 };
 
 export const ArticleImageBlockComponent = (props: Props) => {
-  return <div>ArticleImageBlockComponent</div>;
+  const { block, className } = props;
+  return (
+    <div
+      className={classNames(styles.wrapper, {}, [className])}
+    >
+      <img src={block.src} className={styles.img} />
+      {block.title && <Text text={block.title} align={TextAlign.CENTER} />}
+    </div>
+  );
 };

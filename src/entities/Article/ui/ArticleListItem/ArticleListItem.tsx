4881bbbc2a1c551b37cc5a0a16b3dba2281c19b1
Николай Горичев <nikogorichev812@gmail.com>
2024-11-11 14:25:21 +0300

    add styles for grid styles

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -2,6 +2,8 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { Article, ArticleView } from "../../model/types/article";
 import styles from "./ArticleListItem.module.scss";
 import { Text } from "shared/ui/Text/Text";
+import { Icon } from "shared/ui/Icon/Icon";
+import EyeIcon from "shared/assets/icons/view-20-20.svg";
 
 type Props = {
   className?: string;
@@ -26,9 +28,15 @@ export const ArticleListItem = (props: Props) => {
     <div className={classNames(styles.wrapper, {}, [className, styles[view]])}>
       <div className={styles.card}>
         <div className={styles.imageWrapper}>
-          <img src={article.img} className={styles.img} />
+          <img src={article.img} className={styles.img} alt={article.title} />
           <Text text={article.createdAt} className={styles.date} />
         </div>
+        <div className={styles.infoWrapper}>
+          <Text text={article.type.join(", ")} className={styles.types} />
+          <Text text={String(article.views)} className={styles.views} />
+          <Icon Svg={EyeIcon} />
+        </div>
+        <Text text={article.title} className={styles.title} />
       </div>
     </div>
   );

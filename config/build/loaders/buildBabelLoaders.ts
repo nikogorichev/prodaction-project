fb70bcd5e4dcaa467d3f110c7d3abc3a38deed85
Николай Горичев <nikogorichev812@gmail.com>
2025-03-13 21:06:42 +0300

    modify babel plugin

--- config/build/loaders/buildBabelLoaders.ts
+++ config/build/loaders/buildBabelLoaders.ts
@@ -16,7 +16,7 @@ export const buildBabelLoaders = ({ isDev, isTsx }: BuildBabelLoaderProps) => ({
         ["i18next-extract", { locales: ["ru", "en"], keyAsDefaultValue: true }],
         ["@babel/plugin-transform-typescript", { isTsx }],
         "@babel/plugin-transform-runtime",
-        isTsx && [babelRemovePropsPlugin(), { props: ["data-testid"] }],
+        isTsx && !isDev && [babelRemovePropsPlugin(), { props: ["data-testid"] }],
         isDev && require.resolve("react-refresh/babel"),
       ].filter(Boolean),
     },
